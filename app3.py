18813e5f4cc2efef61cb99e53cee1453818d01c5
Htetko510217 <htetko510217@gmai.com>
2024-06-26 22:55:46 +0700

    feat : use edge_tts for khmer language support

---
+++ app3.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, send_file
+from flask_cors import CORS
+import edge_tts
+import asyncio
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/tts', methods=['POST'])
+def tts():
+    async def async_tts(text, filename):
+        communicate = edge_tts.Communicate(text, 'km-KH-SreymomNeural')
+        await communicate.save(filename)
+
+    data = request.json
+    text = data.get('text')
+
+    if not text:
+        return {"error": "Text is required"}, 400
+
+    filename = "output.mp3"
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(async_tts(text, filename))
+
+    return send_file(filename, as_attachment=True, download_name=filename)
+
+if __name__ == "__main__":
+    app.run(debug=True)
